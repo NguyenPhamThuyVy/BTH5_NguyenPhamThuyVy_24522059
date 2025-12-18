5f8ac2e3e84160d55db6216bf08b73628e0ba33a
NguyenPhamThuyVy <24522059@gm.uit.edu.vn>
2025-12-18 16:24:59 +0700

    Add files via upload

---
+++ Bai06.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace BTH5_BT6
+{
+    partial class Bai06
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flpFonts = new FlowLayoutPanel();
+            SuspendLayout();
+            // 
+            // flpFonts
+            // 
+            flpFonts.AutoScroll = true;
+            flpFonts.FlowDirection = FlowDirection.TopDown;
+            flpFonts.Location = new Point(12, 12);
+            flpFonts.Name = "flpFonts";
+            flpFonts.Size = new Size(495, 584);
+            flpFonts.TabIndex = 0;
+            flpFonts.WrapContents = false;
+            // 
+            // Bai06
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(519, 608);
+            Controls.Add(flpFonts);
+            MaximizeBox = false;
+            Name = "Bai06";
+            Text = "All Fonts";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flpFonts;
+    }
+}
---
+++ Bai06.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Windows.Forms;
+
+namespace BTH5_BT6
+{
+    public partial class Bai06 : Form
+    {
+        public Bai06()
+        {
+            InitializeComponent();
+            LoadFonts();
+        }
+        // Hàm tải Font lên
+        private void LoadFonts()
+        {
+            flpFonts.HorizontalScroll.Enabled = true;
+            flpFonts.HorizontalScroll.Visible = true;
+            InstalledFontCollection installedFonts = new InstalledFontCollection();
+
+            foreach (FontFamily family in installedFonts.Families)
+            { 
+                Label lblFont = new Label();
+                lblFont.Text = family.Name;
+                lblFont.AutoSize = true;
+                lblFont.Padding = new Padding(5);
+                lblFont.Margin = new Padding(5, 5, 20, 5);
+
+                try
+                {
+                    lblFont.Font = new Font(family, 16, FontStyle.Regular);
+                }
+                catch
+                {
+                    continue;
+                }
+                flpFonts.Controls.Add(lblFont);
+                int requiredWidth = lblFont.Right + 200;
+                if (requiredWidth > flpFonts.AutoScrollMinSize.Width)
+                {
+                    flpFonts.AutoScrollMinSize =
+                        new Size(requiredWidth, flpFonts.AutoScrollMinSize.Height);
+                }
+            }
+        }
+    }
+}
